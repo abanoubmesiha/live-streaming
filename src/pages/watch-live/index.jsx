a282b63bd2d74d312e48b6c79cb998291d481238
Abanoub <anagy@app-corp.com>
2022-02-26 22:23:35 +0200

    Move logic from button to useEffect.

--- src/pages/watch-live/index.jsx
+++ src/pages/watch-live/index.jsx
@@ -1,33 +1,40 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { createPeer } from '../../utils/peer';
 
 export default function WatchLive() {
-  useEffect(() => {
+  const [dataChannelState, setDataChannelState] = useState({});
+
+  useEffect(async () => {
     const peer = createPeer('/consumer');
     peer.ontrack = (e) => {
       [document.getElementById('watch-live').srcObject] = e.streams;
     };
     peer.addTransceiver('video', { direction: 'recvonly' });
+    // *
+    const localConnection = new RTCPeerConnection();
+    const dataChannel = localConnection.createDataChannel('channel');
+    dataChannel.onopen = () => {
+      console.log('Connection Opened');
+      setDataChannelState(dataChannel);
+    };
+    dataChannel.onmessage = (e) => console.log(`Just got a message${e.data}`);
+    const offer = await localConnection.createOffer();
+    await localConnection.setLocalDescription(offer);
+
+    const { data } = await axios.post('http://localhost:8000/comments', { offer: localConnection.localDescription });
+    const answerDesc = new RTCSessionDescription(data.answer);
+    localConnection.setRemoteDescription(answerDesc).catch((e) => console.log(e));
   }, []);
   return (
     <section className="watch-live">
       <button
         type="button"
-        onClick={async () => {
-          const localConnection = new RTCPeerConnection();
-          const dataChannel = localConnection.createDataChannel('channel');
-          dataChannel.onopen = () => console.log('Connection Opened');
-          dataChannel.onmessage = (e) => console.log(`Just got a message${e.data}`);
-          const offer = await localConnection.createOffer();
-          await localConnection.setLocalDescription(offer);
-
-          const { data } = await axios.post('http://localhost:8000/comments', { offer: localConnection.localDescription });
-          const answerDesc = new RTCSessionDescription(data.answer);
-          localConnection.setRemoteDescription(answerDesc).catch((e) => console.log(e));
+        onClick={() => {
+          dataChannelState.send('Hi from button');
         }}
       >
-        Test
+        Send a message
       </button>
       <video id="watch-live" controls autoPlay playsInline muted />
     </section>

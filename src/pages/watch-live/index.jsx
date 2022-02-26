8ad60f6c9def9fd8b5931e36dd361332b943ab8d
Abanoub <anagy@app-corp.com>
2022-02-26 11:52:29 +0200

    * Make both pages centerally aligned.

--- src/pages/watch-live/index.jsx
+++ src/pages/watch-live/index.jsx
@@ -1,22 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { createPeer } from '../../utils/peer';
 
 export default function WatchLive() {
+  useEffect(() => {
+    const peer = createPeer('/consumer');
+    peer.ontrack = (e) => {
+      [document.getElementById('watch-live').srcObject] = e.streams;
+    };
+    peer.addTransceiver('video', { direction: 'recvonly' });
+  }, []);
   return (
-    <>
-      <button
-        type="button"
-        onClick={() => {
-          const peer = createPeer('/consumer');
-          peer.ontrack = (e) => {
-            [document.getElementById('watch-live').srcObject] = e.streams;
-          };
-          peer.addTransceiver('video', { direction: 'recvonly' });
-        }}
-      >
-        Watch Live
-      </button>
+    <section className="watch-live">
       <video id="watch-live" controls autoPlay playsInline muted />
-    </>
+    </section>
   );
 }
